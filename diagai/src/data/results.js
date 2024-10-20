adbbd7856d6506228141d25054c7aec7797b0d23
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-10-20 11:11:04 +0100

    Update config file

---
+++ diagai/src/data/results.js
@@ -0,0 +1,81 @@
+const diagonis = [
+  {
+    id: 1,
+    percent: "46%",
+    diagonis: "Malaria",
+    references: [
+      "nigeria_malaria__info_data.pdf",
+      "malaria_symptoms.pdf",
+      "nigeria_malaria_data.pdf",
+      "nigeria_disease_center.pdf",
+    ],
+  },
+  {
+    id: 2,
+    percent: "56%",
+    diagonis: "Fever",
+    references: [
+      "nigeria_fever__info_data.pdf",
+      "fever_symptoms.pdf",
+      "nigeria_fever_data.pdf",
+      "nigeria_disease_center.pdf",
+    ],
+  },
+  {
+    id: 3,
+    percent: "33%",
+    diagonis: "Typhoid",
+    references: [
+      "nigeria_typhoid_info_data.pdf",
+      "typhoid_symptoms.pdf",
+      "nigeria_typhoid_data.pdf",
+      "nigeria_disease_center.pdf",
+    ],
+  },
+  {
+    id: 4,
+    percent: "76%",
+    diagonis: "Pnuemonia",
+    references: [
+      "nigeria_pnuemonia_info_data.pdf",
+      "pnuemonia_symptoms.pdf",
+      "nigeria_pnuemonia_data.pdf",
+      "nigeria_disease_center.pdf",
+    ],
+  },
+  {
+    id: 5,
+    percent: "88%",
+    diagonis: "Cough",
+    references: [
+      "nigeria_cough_info_data.pdf",
+      "cough_symptoms.pdf",
+      "nigeria_cough_data.pdf",
+      "nigeria_disease_center.pdf",
+    ],
+  },
+  {
+    id: 6,
+    percent: "96%",
+    diagonis: "Stress",
+    references: [
+      "nigeria_stress_info_data.pdf",
+      "stress_symptoms.pdf",
+      "nigeria_stress_data.pdf",
+      "nigeria_disease_center.pdf",
+    ],
+  },
+  {
+    id: 7,
+    percent: "25%",
+    diagonis: "Tubercolosis",
+    references: [
+      "nigeria_tuberculosis_info_data.pdf",
+      "tuberculosis_symptoms.pdf",
+      "nigeria_tuberculosis_data.pdf",
+      "nigeria_disease_center.pdf",
+    ],
+  },
+];
+
+module.exports = diagonis
