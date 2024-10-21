1497a0085152a5e83195197d5db9755e2642b337
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-10-21 17:47:12 +0100

    Implement mock data for records component

---
+++ diagai/src/data/records.js
@@ -0,0 +1,58 @@
+const recordList = [
+  {
+    patientId: "001",
+    illness_disease: "Malaria, Corona Virus, Typhoid, Measles, Monkey Pox",
+    symptoms: "Fever, Headache, Fatigue",
+    references: [
+      "nigeria_center_for_disease_control.pdf",
+      "world_health_organization_info.pdf",
+    ],
+  },
+  {
+    patientId: "002",
+    illness_disease: "Corona Virus, Malaria, Typhoid, Measles, Monkey Pox",
+    symptoms: "Headache, Fever, Fatigue",
+    references: [
+      "world_health_organization_info.pdf",
+      "nigeria_center_for_disease_control.pdf",
+    ],
+  },
+  {
+    patientId: "003",
+    illness_disease: "Typhoid, Measles, Monkey Pox, Malaria, Corona Virus",
+    symptoms: "Fatigue, Fever, Headache",
+    references: [
+      "nigeria_center_for_disease_control.pdf",
+      "world_health_organization_info.pdf",
+    ],
+  },
+  {
+    patientId: "004",
+    illness_disease: "Measles, Monkey Pox, Malaria, Corona Virus, Typhoid",
+    symptoms: "Fever, Headache, Fatigue",
+    references: [
+      "world_health_organization_info.pdf",
+      "nigeria_center_for_disease_control.pdf",
+    ],
+  },
+  {
+    patientId: "005",
+    illness_disease: "Monkey Pox, Malaria, Corona Virus, Typhoid, Measles",
+    symptoms: "Headache, Fever, Fatigue",
+    references: [
+      "nigeria_center_for_disease_control.pdf",
+      "world_health_organization_info.pdf",
+    ],
+  },
+  {
+    patientId: "006",
+    illness_disease: "Malaria, Corona Virus, Typhoid, Measles, Monkey Pox",
+    symptoms: "Fever, Headache, Fatigue",
+    references: [
+      "nigeria_center_for_disease_control.pdf",
+      "world_health_organization_info.pdf",
+    ],
+  },
+];
+
+module.exports = recordList
